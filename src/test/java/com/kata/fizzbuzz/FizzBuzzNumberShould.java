f2ab70f7cd54075fc6c29bd86f38a8e11d0949f8
Deep Muni <deep.muni94@gmail.com>
2024-12-14 15:29:19 +0530

    Reformats test to parameterized test

--- src/test/java/com/kata/fizzbuzz/FizzBuzzNumberShould.java
+++ src/test/java/com/kata/fizzbuzz/FizzBuzzNumberShould.java
@@ -2,25 +2,15 @@ package com.kata.fizzbuzz;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class FizzBuzzNumberShould {
 
-  @Test
-  void convert_1_to_1() {
-    FizzBuzzNumber number = new FizzBuzzNumber(1);
-    assertThat(number.convert()).isEqualTo("1");
-  }
-
-  @Test
-  void convert_2_to_2() {
-    FizzBuzzNumber number = new FizzBuzzNumber(2);
-    assertThat(number.convert()).isEqualTo("2");
-  }
-
-  @Test
-  void convert_3_to_Fizz() {
-    FizzBuzzNumber number = new FizzBuzzNumber(3);
-    assertThat(number.convert()).isEqualTo("Fizz");
+  @ParameterizedTest
+  @CsvSource({"1, 1", "2, 2", "3, Fizz"})
+  void convert_number_to_fizz_buzz_string(Integer value, String expected) {
+    FizzBuzzNumber number = new FizzBuzzNumber(value);
+    assertThat(number.convert()).isEqualTo(expected);
   }
 }
