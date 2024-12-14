b0b2706f30ecf3e2ef9991065402ef3a3246404a
Deep Muni <deep.muni94@gmail.com>
2024-12-14 15:33:52 +0530

    Reformats the condition to check if number is divisible by 3

--- src/main/java/com/kata/fizzbuzz/FizzBuzzNumber.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzzNumber.java
@@ -9,7 +9,7 @@ public class FizzBuzzNumber {
   }
 
   public String convert() {
-    if (number.equals(3) || number.equals(6) || number.equals(9)) {
+    if (number % 3 == 0) {
       return "Fizz";
     }
     if (number.equals(5)) {
